9f1f20a2ff6b8587dc00223de66429ebed605fea
Stanislav Tyshchenko <stas7t@gmail.com>
2017-11-02 02:23:32 +0200

    Fix abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,17 +6,17 @@ class Ability
     if user.is_a? Admin
       can :manage, :all
     elsif user.is_a?(User) && user.persisted?
-      can :read, [Author, Book, Category, Coupon, Delivery]
+      can :read, [Author, Book, Category, ShoppingCart::Coupon, ShoppingCart::Delivery]
       can :read, Review
       can :create, Review, user_id: user.id
-      can %i[read create update], [Order, Address, CreditCard], user_id: user.id
-      can :create, OrderItem
-      can %i[update destroy], OrderItem, id: session[:order_item_ids].to_a
+      can %i[read create update], [ShoppingCart::Order, ShoppingCart::Address, ShoppingCart::CreditCard], user_id: user.id
+      can :create, ShoppingCart::OrderItem
+      can %i[update destroy], ShoppingCart::OrderItem, id: session[:order_item_ids].to_a
       can :manage, User, id: user.id
     else
       can :read, [Author, Book, Category, Review]
-      can :create, OrderItem
-      can %i[update destroy], OrderItem, id: session[:order_item_ids].to_a
+      can :create, ShoppingCart::OrderItem
+      can %i[update destroy], ShoppingCart::OrderItem, id: session[:order_item_ids].to_a
     end
   end
 end
