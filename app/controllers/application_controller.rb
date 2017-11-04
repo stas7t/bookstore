96becc4058cd714d8bfc42d3bf60a2c7fc3ee416
Stanislav Tyshchenko <stas7t@gmail.com>
2017-11-05 00:52:21 +0200

    Remove helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_order
+  include ShoppingCart::CurrentSession
 
-  protected
-=begin
-  def after_sign_in_path_for(resource)
-    if cookies[:from_checkout]
-      cookies.delete :from_checkout
-      checkout_path(:addresses)
-    else
-      super
-    end
-  end
-=end
+  helper ShoppingCart::Engine.helpers
+
   private
-
-  def current_order
-    return ShoppingCart::Order.new unless session[:order_id]
-    ShoppingCart::Order.find(session[:order_id])
-  end
 
   def current_ability
     @current_ability ||= Ability.new(current_user, session)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,6 @@ module ApplicationHelper
     Category.order('name')
   end
 
-  def cart_items_count
-    return ShoppingCart::OrderItem.where(id: session[:order_item_ids]).sum(:quantity) if session[:order_item_ids]
-    current_order.order_items.sum(:quantity)
-  end
-
   def number_to_euro(amount)
     number_to_currency(amount, unit: '€')
   end
